7fab39699cb70d4be82050fbee3a983e245ed692
Cédric Belin <cedx@outlook.com>
2026-07-27 21:41:57 +0200

    Add the `BadRequest` class

---
+++ Sources/Authentication/BadRequest.cs
@@ -0,0 +1,12 @@
+namespace Mc2it.Agicap.Authentication;
+
+/// <summary>
+///	Represents a bad request.
+/// </summary>
+public sealed class BadRequest {
+
+	/// <summary>
+	/// The error message.
+	/// </summary>
+	public string Error { get; set; } = "";
+}
