0e99698bd1c5760bd99b5fddaec9bf699797bca2
Cédric Belin <cedx@outlook.com>
2026-07-28 14:13:09 +0200

    Add tests for the `Entity` and `Organization` classes

---
+++ Tests/Organizations/Entity.Tests.cs
@@ -0,0 +1,19 @@
+namespace Mc2it.Agicap.Organizations;
+
+using System.Text.Json;
+
+/// <summary>
+/// Tests the features of the <see cref="Entity"/> class.
+/// </summary>
+[TestClass]
+public sealed class EntityTests {
+
+	[TestMethod]
+	public void FromJson() {
+		var json = File.ReadAllText(Path.Join(AppContext.BaseDirectory, "../Resources/Organizations/Entity.json"));
+		var entity = JsonSerializer.Deserialize<Entity>(json, JsonSerializerOptions.Web)!;
+		AreEqual("FR", entity.Country);
+		AreEqual(666, entity.Id);
+		AreEqual("My Entity", entity.Name);
+	}
+}
---
+++ Tests/Organizations/Organization.Tests.cs
@@ -0,0 +1,18 @@
+namespace Mc2it.Agicap.Organizations;
+
+using System.Text.Json;
+
+/// <summary>
+/// Tests the features of the <see cref="Organization"/> class.
+/// </summary>
+[TestClass]
+public sealed class OrganizationTests {
+
+	[TestMethod]
+	public void FromJson() {
+		var json = File.ReadAllText(Path.Join(AppContext.BaseDirectory, "../Resources/Organizations/Organization.json"));
+		var organization = JsonSerializer.Deserialize<Organization>(json, JsonSerializerOptions.Web)!;
+		AreEqual(new Guid("3ebb0163-6ac8-449d-a34b-496244f380a1"), organization.Id);
+		AreEqual("My Company", organization.Name);
+	}
+}
