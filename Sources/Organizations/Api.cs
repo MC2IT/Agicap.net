919d950a16250841c084863fa642ff2c281cb24f
Cédric Belin <cedx@outlook.com>
2026-07-28 21:35:09 +0200

    Update the documentation [skip ci]

--- Sources/Organizations/Api.cs
+++ Sources/Organizations/Api.cs
@@ -31,22 +31,22 @@ public class Api(Client client) {
 	}
 
 	/// <summary>
-	/// Fetches the list of all organizations.
+	/// Fetches the organization list.
 	/// </summary>
 	/// <param name="pageNumber">The page number.</param>
 	/// <param name="pageSize">The number of elements per page.</param>
 	/// <param name="cancellationToken">The token to cancel the operation.</param>
-	/// <returns>The list of all organizations.</returns>
+	/// <returns>The organization list.</returns>
 	public PaginatedList<Organization> GetOrganizations(int? pageNumber = null, int? pageSize = null, CancellationToken cancellationToken = default) =>
 		GetOrganizationsAsync(pageNumber, pageSize, cancellationToken).GetAwaiter().GetResult();
 
 	/// <summary>
-	/// Fetches the list of all organizations.
+	/// Fetches the organization list.
 	/// </summary>
 	/// <param name="pageNumber">The page number.</param>
 	/// <param name="pageSize">The number of elements per page.</param>
 	/// <param name="cancellationToken">The token to cancel the operation.</param>
-	/// <returns>The list of all organizations.</returns>
+	/// <returns>The organization list.</returns>
 	public async Task<PaginatedList<Organization>> GetOrganizationsAsync(int? pageNumber = null, int? pageSize = null, CancellationToken cancellationToken = default) {
 		var queryString = new Dictionary<string, object?> { ["pageNumber"] = pageNumber, ["pageSize"] = pageSize };
 		return await client.GetAsync<PaginatedList<Organization>>("organizations/v1", queryString, cancellationToken);
