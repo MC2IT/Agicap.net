7038c68ae2e41728042a55b1c97ac995a01bdcdb
Cédric Belin <cedx@outlook.com>
2026-07-29 11:58:46 +0200

    Update the tests

--- Tests/Client.Tests.cs
+++ Tests/Client.Tests.cs
@@ -13,13 +13,12 @@ public sealed class ClientTests(TestContext testContext) {
 	public async Task Authenticate() {
 		// It should return a new access token.
 		var client = new Client(Environment.GetEnvironmentVariable("AGICAP_CLIENT_ID")!, Environment.GetEnvironmentVariable("AGICAP_CLIENT_SECRET")!);
-		var scopes = new[] { Scopes.PublicApi, Scopes.ImportPaymentFiles, Scopes.ManagePaymentBeneficiaries };
 		IsFalse(client.IsAuthenticated);
 
-		var accessToken = await client.AuthenticateAsync(scopes, testContext.CancellationToken);
+		var accessToken = await client.AuthenticateAsync(Scopes.All, testContext.CancellationToken);
 		IsTrue(client.IsAuthenticated);
 		IsFalse(accessToken.HasExpired);
-		AreSequenceEqual(scopes, accessToken.Scopes);
+		AreSequenceEqual(Scopes.All, accessToken.Scopes);
 		AreEqual("Bearer", accessToken.Type);
 		MatchesRegex(@"^[A-Z\d]{64,}", accessToken.Value);
 
