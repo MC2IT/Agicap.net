761efd9e00421233cec3e1f986b83f8102ed0e50
Cédric Belin <cedx@outlook.com>
2026-07-30 17:57:27 +0200

    Update the tests

--- Tests/Payments/BeneficiaryApi.Tests.cs
+++ Tests/Payments/BeneficiaryApi.Tests.cs
@@ -1,5 +1,7 @@
 namespace Mc2it.Agicap.Payments;
 
+using System.Net;
+
 /// <summary>
 /// Tests the features of the <see cref="BeneficiaryApi"/> class.
 /// </summary>
@@ -19,13 +21,28 @@ public sealed class BeneficiaryApiTests(TestContext testContext) {
 			PostalAddress = new() { City = "Fabrègues", Country = "FR", StreetName = "Rue Gine" }
 		};
 
+		// It should create the specified beneficiary.
 		AreEqual(Guid.Empty, beneficiary.Id);
 		await api.CreateAsync(beneficiary, testContext.CancellationToken);
 		AreNotEqual(Guid.Empty, beneficiary.Id);
 
+		// TODO It should throw an exception if the beneficiary already exists.
+		// try {
+		// 	await api.CreateAsync(beneficiary, testContext.CancellationToken);
+		// 	Fail("The exception was not thrown as planned.");
+		// }
+		// catch (HttpResponseException e) {
+		// 	AreEqual(HttpStatusCode.Conflict, e.StatusCode);
+		// 	IsNotNull(e.ProblemDetails);
+		// 	MatchesRegex("beneficiary.*MC2IT.*exist", e.ProblemDetails.Title);
+		// }
+
+		// It should update the specified beneficiary.
 		beneficiary.PostalAddress.Number = "29";
 		beneficiary.PostalAddress.ZipCode = "34690";
 		await api.UpdateAsync(beneficiary, testContext.CancellationToken);
+
+		// It should delete the specified beneficiary.
 		await api.DeleteAsync(beneficiary, testContext.CancellationToken);
 	}
 
