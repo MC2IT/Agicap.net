7b2f1573fc5bda1725d7ca7cba3f2eab1ce1fcb3
Cédric Belin <cedx@outlook.com>
2026-08-05 16:47:18 +0200

    Add the skeletons of the "ChartOfAccounts" APIs [skip ci]

---
+++ Sources/ChartOfAccounts/AccountingAccountApi.cs
@@ -0,0 +1,8 @@
+namespace Mc2it.Agicap.ChartOfAccounts;
+
+/// <summary>
+/// Manages the accounting accounts of the chart of accounts.
+/// </summary>
+/// <param name="client">The Agicap API client.</param>
+public class AccountingAccountApi(Client client) {
+}
---
+++ Sources/ChartOfAccounts/ThirdPartyApi.cs
@@ -0,0 +1,8 @@
+namespace Mc2it.Agicap.ChartOfAccounts;
+
+/// <summary>
+/// Manages the third-parties of the chart of accounts.
+/// </summary>
+/// <param name="client">The Agicap API client.</param>
+public class ThirdPartyApi(Client client) {
+}
