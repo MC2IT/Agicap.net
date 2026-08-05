0703b8810144e6f40b0d880a54d3edc90ce18642
Cédric Belin <cedx@outlook.com>
2026-08-05 17:10:06 +0200

    Add the `AccountingAccountList` class

---
+++ Sources/ChartOfAccounts/AccountingAccountList.cs
@@ -0,0 +1,12 @@
+namespace Mc2it.Agicap.ChartOfAccounts;
+
+/// <summary>
+/// Represents a list of accounting account numbers.
+/// </summary>
+public class AccountingAccountList {
+
+	/// <summary>
+	/// The list items.
+	/// </summary>
+	public IList<string> AccountingAccountNumbers { get; set; } = [];
+}
