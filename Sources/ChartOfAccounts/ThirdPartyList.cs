0c7452aed87302e216db15873495c40a326e52dd
Cédric Belin <cedx@outlook.com>
2026-08-05 17:10:26 +0200

    Add the `ThirdPartyList` class [skip ci]

---
+++ Sources/ChartOfAccounts/ThirdPartyList.cs
@@ -0,0 +1,12 @@
+namespace Mc2it.Agicap.ChartOfAccounts;
+
+/// <summary>
+/// Represents a list of third-party codes.
+/// </summary>
+public class ThirdPartyList {
+
+	/// <summary>
+	/// The list items.
+	/// </summary>
+	public IList<string> ThirdPartyCodes { get; set; } = [];
+}
