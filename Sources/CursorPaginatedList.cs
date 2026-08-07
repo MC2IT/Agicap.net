13a6427056bfea930d0047ea2f5bca8081483c72
Cédric Belin <cedx@outlook.com>
2026-08-07 15:37:46 +0200

    Add the `CursorPaginatedList` class [skip ci]

---
+++ Sources/CursorPaginatedList.cs
@@ -0,0 +1,18 @@
+namespace Mc2it.Agicap;
+
+/// <summary>
+/// Represents a strongly typed list of objects, with a cursor allowing access to the next page of results.
+/// </summary>
+/// <typeparam name="T">The type of items in the list.</typeparam>
+public class CursorPaginatedList<T> {
+
+	/// <summary>
+	/// The list items.
+	/// </summary>
+	public IList<T> Items { get; set; } = [];
+
+	/// <summary>
+	/// A cursor to pass back to fetch the next page, or <see langword="null"/> when the last page is reached.
+	/// </summary>
+	public string? NextCursor { get; set; }
+}
