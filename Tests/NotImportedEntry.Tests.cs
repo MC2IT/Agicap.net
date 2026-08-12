00e2761d84f5c81ade2e4bd15b93ae045606e1d4
Cédric Belin <cedx@outlook.com>
2026-08-12 11:38:03 +0200

    Add tests for the `NotImportedEntry` class

---
+++ Tests/NotImportedEntry.Tests.cs
@@ -0,0 +1,23 @@
+namespace Mc2it.Agicap;
+
+using System.Text.Json;
+
+/// <summary>
+/// Tests the features of the <see cref="NotImportedEntry"/> class.
+/// </summary>
+[TestClass]
+public sealed class NotImportedEntryTests {
+
+	[TestMethod]
+	public void ToJson() {
+		var guid = Guid.NewGuid();
+		var notImportedEntry = new NotImportedEntry {
+			EntryAgicapUniqueId = guid,
+			Errors = { new() { ErrorMessage = "An error occurred.", ErrorType = NotImportedEntryErrorTypes.UnknownVatAccount } }
+		};
+
+		var json = JsonSerializer.Serialize(notImportedEntry, JsonSerializerOptions.Web);
+		Contains($"\"entryAgicapUniqueId\":\"{guid}\"", json);
+		Contains("\"errors\":[{", json);
+	}
+}
