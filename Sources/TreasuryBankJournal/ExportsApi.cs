4fcc87ea8242e73f9fe5c8cc09028441b768bce2
Cédric Belin <cedx@outlook.com>
2026-08-13 12:04:50 +0200

    Update the `BankJournalEntry` class [skip ci]

--- Sources/TreasuryBankJournal/ExportsApi.cs
+++ Sources/TreasuryBankJournal/ExportsApi.cs
@@ -1,7 +1,7 @@
 namespace Mc2it.Agicap.TreasuryBankJournal;
 
 /// <summary>
-/// Manages the treasury bank journal.
+/// Manages the exports of the treasury bank journal.
 /// </summary>
 /// <param name="client">The Agicap API client.</param>
 /// <param name="entityId">The entity identifier.</param>
