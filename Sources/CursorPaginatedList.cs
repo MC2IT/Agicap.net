eadba85630c6b738e95b0e0834d4cd737eef7d98
Cédric Belin <cedx@outlook.com>
2026-08-13 17:15:30 +0200

    Update the documentation [skip ci]

--- Sources/CursorPaginatedList.cs
+++ Sources/CursorPaginatedList.cs
@@ -1,7 +1,7 @@
 namespace Mc2it.Agicap;
 
 /// <summary>
-/// Represents a strongly typed list of objects, with a cursor allowing access to the next page of results.
+/// Represents a strongly typed list of objects, with a cursor allowing access to the next/previous page of results.
 /// </summary>
 /// <typeparam name="T">The type of items in the list.</typeparam>
 public class CursorPaginatedList<T> {
