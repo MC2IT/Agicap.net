870c4e8c4944f1343d506272da0c406751e6c5ca
Cédric Belin <cedx@outlook.com>
2026-08-13 12:28:28 +0200

    Add the `ExportApi.Create()` method [skip ci]

--- Sources/TreasuryBankJournal/ExportApi.cs
+++ Sources/TreasuryBankJournal/ExportApi.cs
@@ -1,5 +1,7 @@
 namespace Mc2it.Agicap.TreasuryBankJournal;
 
+using System.Net.Http.Json;
+
 /// <summary>
 /// Manages the exports of the treasury bank journal.
 /// </summary>
@@ -11,6 +13,30 @@ public class ExportApi(Client client, int entityId) {
 	/// The relative URI of the API endpoint.
 	/// </summary>
 	private readonly string requestUri = $"treasury-bank-journal/v1/entities/{entityId}/exports";
+
+	/// <summary>
+	/// Exports all bank journal entries ready to be exported.
+	/// </summary>
+	/// <param name="exportId">The identifier of the bank journal export.</param>
+	/// <param name="exportRequest">Optional export parameters allowing to set where to start.</param>
+	/// <param name="cancellationToken">The token to cancel the operation.</param>
+	/// <returns>The identifier of the newly started synchronization.</returns>
+	public BankJournalExport Create(Guid? exportId = null, BankJournalExportRequest? exportRequest = null, CancellationToken cancellationToken = default) =>
+		CreateAsync(exportId, exportRequest, cancellationToken).GetAwaiter().GetResult();
+
+	/// <summary>
+	/// Exports all bank journal entries ready to be exported.
+	/// </summary>
+	/// <param name="exportId">The identifier of the bank journal export.</param>
+	/// <param name="exportRequest">Optional export parameters allowing to set where to start.</param>
+	/// <param name="cancellationToken">The token to cancel the operation.</param>
+	/// <returns>The identifier of the newly started synchronization.</returns>
+	public async Task<BankJournalExport> CreateAsync(Guid? exportId = null, BankJournalExportRequest? exportRequest = null, CancellationToken cancellationToken = default) {
+		exportId ??= Guid.CreateVersion7();
+		var content = exportRequest is null ? null : new { CurrentExportCounts = exportRequest };
+		using var response = await client.PostAsync($"{requestUri}/{exportId}", content, cancellationToken: cancellationToken);
+		return (await response.Content.ReadFromJsonAsync<BankJournalExport>(cancellationToken))!;
+	}
 
 	/// <summary>
 	/// Fetches the bank journal export with the specified identifier.
