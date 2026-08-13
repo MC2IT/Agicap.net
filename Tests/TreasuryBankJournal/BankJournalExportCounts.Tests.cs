0a3bdb1657d802779380f5dcabaa5b991d3eda51
Cédric Belin <cedx@outlook.com>
2026-08-13 17:39:15 +0200

    Code formatting [skip ci]

--- Tests/TreasuryBankJournal/BankJournalExportCounts.Tests.cs
+++ Tests/TreasuryBankJournal/BankJournalExportCounts.Tests.cs
@@ -10,12 +10,12 @@ public sealed class BankJournalExportCountsTests {
 
 	[TestMethod]
 	public void ToJson() {
-		var exportRequest = new BankJournalExportCounts {
+		var exportCounts = new BankJournalExportCounts {
 			CurrentBankJournalEntriesCountInYear = 666,
 			CurrentBankJournalsCountInYear = 123
 		};
 
-		var json = JsonSerializer.Serialize(exportRequest, JsonSerializerOptions.Web);
+		var json = JsonSerializer.Serialize(exportCounts, JsonSerializerOptions.Web);
 		Contains("\"currentBankJournalEntriesCountInYear\":666", json);
 		Contains("\"currentBankJournalsCountInYear\":123", json);
 	}
