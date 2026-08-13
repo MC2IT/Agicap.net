6d47df575fc46f2372f8ad78f01a028551879125
Cédric Belin <cedx@outlook.com>
2026-08-13 12:28:07 +0200

    Add the `BankJournalExportRequest` class

---
+++ Sources/TreasuryBankJournal/BankJournalExportRequest.cs
@@ -0,0 +1,17 @@
+namespace Mc2it.Agicap.TreasuryBankJournal;
+
+/// <summary>
+/// Optional export parameters allowing to set where to start.
+/// </summary>
+public class BankJournalExportRequest {
+
+	/// <summary>
+	/// The number of bank journal entries previously created (starts at 1).
+	/// </summary>
+	public int CurrentBankJournalEntriesCountInYear { get; set; }
+
+	/// <summary>
+	/// The number of bank journal previously created (starts at 1).
+	/// </summary>
+	public int CurrentBankJournalsCountInYear { get; set; }
+}
