2e7dbdf38902268b1bcf8eaabaf7f0db42f53e03
Cédric Belin <cedx@outlook.com>
2026-08-13 12:01:25 +0200

    Add the `ExportsApi.Read()` method [skip ci]

--- Sources/TreasuryBankJournal/ExportsApi.cs
+++ Sources/TreasuryBankJournal/ExportsApi.cs
@@ -13,6 +13,24 @@ public class ExportsApi(Client client, int entityId) {
 	private readonly string requestUri = $"treasury-bank-journal/v1/entities/{entityId}/exports";
 
 	/// <summary>
+	/// Fetches the bank journal export with the specified identifier.
+	/// </summary>
+	/// <param name="exportId">The identifier of the bank journal export.</param>
+	/// <param name="cancellationToken">The token to cancel the operation.</param>
+	/// <returns>The bank journal export with the specified identifier.</returns>
+	public BankJournalExport Read(Guid exportId, CancellationToken cancellationToken = default) =>
+		ReadAsync(exportId, cancellationToken).GetAwaiter().GetResult();
+
+	/// <summary>
+	/// Fetches the bank journal export with the specified identifier.
+	/// </summary>
+	/// <param name="exportId">The identifier of the bank journal export.</param>
+	/// <param name="cancellationToken">The token to cancel the operation.</param>
+	/// <returns>The bank journal export with the specified identifier.</returns>
+	public async Task<BankJournalExport> ReadAsync(Guid exportId, CancellationToken cancellationToken = default) =>
+		await client.GetAsync<BankJournalExport>($"{requestUri}/{exportId}", cancellationToken: cancellationToken);
+
+	/// <summary>
 	/// Fetches a list of short summaries of bank journal entries from previous exports.
 	/// </summary>
 	/// <param name="cancellationToken">The token to cancel the operation.</param>
